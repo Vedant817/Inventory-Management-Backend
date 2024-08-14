1681665797280e63b8c677b3177c66e0f8fd0364
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-08-14 16:13:55 +0530

    Server Updated

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoDB from "./db/mongoDB.js";
+import categoryRouter from "./routes/category.routes.js";
+import itemRouter from "./routes/items.routes.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
+app.use(cors({
+    origin: '*'
+}));
+app.use(express.json());
+
+app.use('/api/category', categoryRouter);
+app.use('/api/item', itemRouter);
+
 const port = process.env.PORT || 3000;
 
 mongoDB().then(() => {
